3e479a107fa7361f6d4f73e569ca3e55dc6cf274
ravitejam66 <raviteja.m@SSLTUIODC.LOCAL>
2018-11-27 16:57:05 +0530

    overlay design created

--- src/app/components/main/author/author.component.ts
+++ src/app/components/main/author/author.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./author.component.css']
 })
 export class AuthorComponent implements OnInit {
-
+   newSection: boolean;
+   submitSection: boolean;
+   mainSection:boolean;
   constructor() { }
 
   ngOnInit() {
+    this.mainSection =  true;
+    this.newSection = false;
+    this.submitSection = false;
+  }
+
+  callNewsection(param){
+   if(param  == 'new'){
+    this.newSection = true;
+    this.submitSection = false;
+    this.mainSection =  false;
+   } else if(param  == 'submit'){
+    this.newSection = false;
+    this.submitSection = true;
+    this.mainSection =  false;
+   }
+   else{
+    this.newSection = false;
+    this.submitSection = false;
+    this.mainSection =  true;
+   }
   }
 
 }
--- src/app/components/main/dashboard/dashboard.component.ts
+++ src/app/components/main/dashboard/dashboard.component.ts
@@ -11,32 +11,9 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $(function() {
-      $('#zoomBtn').click(function() {
-        $('.zoom-btn-sm').toggleClass('scale-out');
-        if (!$('.zoom-card').hasClass('scale-out')) {
-          $('.zoom-card').toggleClass('scale-out');
-        }
-      });
-      $('.zoom-btn-sm').click(function() {
-        var btn = $(this);
-        var card = $('.zoom-card');
-        if ($('.zoom-card').hasClass('scale-out')) {
-          $('.zoom-card').toggleClass('scale-out');
-        }
-        if (btn.hasClass('zoom-btn-person')) {
-          card.css('background-color', '#d32f2f');
-        } else if (btn.hasClass('zoom-btn-doc')) {
-          card.css('background-color', '#fbc02d');
-        } else if (btn.hasClass('zoom-btn-tangram')) {
-          card.css('background-color', '#388e3c');
-        } else if (btn.hasClass('zoom-btn-report')) {
-          card.css('background-color', '#1976d2');
-        } else {
-          card.css('background-color', '#7b1fa2');
-        }
-      });
-    });
+   $(function(){
+     
+   });
   }
 
 }
