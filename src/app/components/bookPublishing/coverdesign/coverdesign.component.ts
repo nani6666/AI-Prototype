378ffb736d6dee14192e7c2f4bd6d301a5d322e2
Raviteja-at-680082445736 <raviteja.m6666@gmail.com>
2018-12-10 23:55:10 +0530

    design and data is changed in cover page

--- src/app/components/bookPublishing/coverdesign/coverdesign.component.ts
+++ src/app/components/bookPublishing/coverdesign/coverdesign.component.ts
@@ -11,23 +11,9 @@ export class CoverdesignComponent implements OnInit {
 
   ngOnInit() {
 
-  this.product = [
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJYPbXmuUF_mGdG-hVFYsdMBKthUgDqJlk2OTIWzk66ittTHw1',
-];
-}
+  this.product = ['5*8' , '5.06*7.81' , '5.25*8' , '5.5*8.5' ,
+                  '6*9' , '6.14*9.21' , '6.69*9.61' , '7*10' ,
+                  '7.44*9.69' , '7.5*9.25' , '8*10' , '8.25*6' ,
+                  '8.25*8.25' , '10*8' , '6*8' , '9*5'];
+  }
 }
