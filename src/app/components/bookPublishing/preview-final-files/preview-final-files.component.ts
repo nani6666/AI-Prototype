d91faf70585e3aa6502e2565f01e6c5764af866e
luckynani28 <venkatalakshmikande@gmail.com>
2018-12-12 16:51:06 +0530

    sidenav commit

--- src/app/components/bookPublishing/preview-final-files/preview-final-files.component.ts
+++ src/app/components/bookPublishing/preview-final-files/preview-final-files.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preview-final-files.component.css']
 })
 export class PreviewFinalFilesComponent implements OnInit {
+
  date: any;
  finishedParam: boolean;
   constructor() { }
@@ -13,6 +14,7 @@ export class PreviewFinalFilesComponent implements OnInit {
   ngOnInit() {
     this.finishedParam = true;
     this.date =  new Date();
+
   }
 
 }
