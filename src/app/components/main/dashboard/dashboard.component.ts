4bf88b2fec58e67d0e4f83d051f20412d1ae2550
Raviteja-at-680082445736 <raviteja.m6666@gmail.com>
2019-02-24 11:52:34 +0530

    code refactored

--- src/app/components/main/dashboard/dashboard.component.ts
+++ src/app/components/main/dashboard/dashboard.component.ts
@@ -10,32 +10,43 @@ import { Router } from '@angular/router';
 export class DashboardComponent implements OnInit {
   today = new Date();
   booksWritten: any[] ;
-  booksWrittenList: any[];
+  booksWrittenListRow1: any[];
+  booksWrittenListRow2: any[];
   constructor(private router: Router) { }
 
   ngOnInit() {
     const bookgetData = JSON.parse(localStorage.getItem('booksArray'));
     this.booksWritten = bookgetData;
     console.log(bookgetData);
-    this.booksWrittenList = [{ 'row1': [
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' },
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' },
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' },
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' },
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' },
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' }
-                                        ]
-                              },
-                             { 'row2': [
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' },
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' },
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' },
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' },
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' },
-                                         {'img': '', 'bookname': '', 'author': 'Mustang' }
-                                       ]
-                             }
-                            ];
+    this.booksWrittenListRow1 =  [
+                                   {'img': '../../../../assets/images/booksWritten/4hourworkweek.jpg',
+                                   'bookname': 'The 4 hour work week', 'author': 'Timothy Ferriss' },
+                                   {'img': '../../../../assets/images/booksWritten/7habits.jpg',
+                                   'bookname': 'The 7 Habits of Highly Effective People', 'author': 'Stephen R. Covey' },
+                                   {'img': '../../../../assets/images/booksWritten/artwithstart.jpg',
+                                   'bookname': 'The Art of the Start 2.0', 'author': 'Guy Kawasaki' },
+                                   {'img': '../../../../assets/images/booksWritten/emyth.jpg',
+                                   'bookname': 'The E-Myth Revisited', 'author': 'Michael E. Gerber' },
+                                   {'img': '../../../../assets/images/booksWritten/emythcontractor.jpg',
+                                   'bookname': 'The E-Myth Contractor', 'author': 'Michael E. Gerber' },
+                                   {'img': '../../../../assets/images/booksWritten/rework.jpeg',
+                                   'bookname': 'ReWork', 'author': 'Jason Fried' },
+                                 ] ;
+     this.booksWrittenListRow2 =  [
+                                    {'img': '../../../../assets/images/booksWritten/startwithwhy.png',
+                                     'bookname': 'Start With Why', 'author': 'Simon Sinek' },
+                                     {'img': '../../../../assets/images/booksWritten/the$100.jpg',
+                                     'bookname': 'The $100 Startup', 'author': 'Chris Guillebeau' },
+                                     {'img': '../../../../assets/images/booksWritten/thehardthing.jpeg',
+                                     'bookname': 'The Hard Thing About Hard Things', 'author': 'Ben Horowitz' },
+                                     {'img': '../../../../assets/images/booksWritten/theleanStartup.jpg',
+                                      'bookname': 'The Lean Startup', 'author': 'Eric Ries' },
+                                     {'img': '../../../../assets/images/booksWritten/thinkandgrowrich.jpg',
+                                      'bookname': 'Think and Grow Rich!', 'author': 'Napoleon Hill' },
+                                     {'img': '../../../../assets/images/booksWritten/zerotoone.jpg',
+                                      'bookname': 'Zero to One', 'author': 'Peter Thiel' },
+                                  ];
+
    $(function() {
 
    });
